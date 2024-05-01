5510150812554d32c05f33a8f0248b1b56858d97
Abdelrahman Khaled <52962808+Abdo404Khaled@users.noreply.github.com>
2024-05-01 09:57:21 +0300

    Time: 24 ms (73.70%) | Memory: 11.8 MB (40.18%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -8,9 +8,9 @@ class Solution(object):
         low, high = 0, n-1
         while low <= high:
             mid = ((high-low)//2) + low
-            if mid < n-1 and nums[mid] < nums[mid+1]:
-                low = mid+1
-            elif mid > 0 and nums[mid] < nums[mid-1]:
+            if mid > 0 and nums[mid] < nums[mid-1]:
                 high = mid-1
+            elif mid < n-1 and nums[mid] < nums[mid+1]:
+                low = mid+1  
             else:
                 return mid
