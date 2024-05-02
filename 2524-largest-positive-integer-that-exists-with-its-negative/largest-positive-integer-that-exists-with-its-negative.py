f1c41c0d1a96d1d369d43bd9ef6e20203f9dae82
Abdelrahman Khaled <52962808+Abdo404Khaled@users.noreply.github.com>
2024-05-02 12:50:16 +0300

    Time: 89 ms (81.87%) | Memory: 11.9 MB (18.13%) - LeetSync

--- 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.py
+++ 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.py
@@ -5,12 +5,12 @@ class Solution(object):
         :rtype: int
         """
         st = set()
+        max_num = -float('inf')
+
         for num in nums:
-            st.add(num)
-        max_num = -1001
-        for num in nums:
-            if num >0:
-                if -1*num in st:
-                    max_num = max(max_num,num)
-        if max_num ==-1001:return -1
-        else:return max_num
+            if -num in st:
+                max_num = max(max_num, -num if num < 0 else num)
+            else:
+                st.add(num)
+
+        return max_num if max_num != -float('inf') else -1
