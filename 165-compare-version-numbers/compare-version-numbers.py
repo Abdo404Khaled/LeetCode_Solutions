1995e2eb1f1244ce74bd71d05d4636e6a5132aa1
Abdelrahman Khaled <52962808+Abdo404Khaled@users.noreply.github.com>
2024-05-03 17:44:01 +0300

    Time: 10 ms (80.98%) | Memory: 11.7 MB (72.20%) - LeetSync

---
+++ 165-compare-version-numbers/compare-version-numbers.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def compareVersion(self, version1, version2):
+        """
+        :type version1: str
+        :type version2: str
+        :rtype: int
+        """
+        parts1, parts2 = version1.split("."), version2.split(".")
+        n, m = len(parts1), len(parts2)
+        i, j = n - 1, m - 1
+        while parts1 != [] and int(parts1[i]) == 0:
+            parts1.pop()
+            i -= 1
+        while parts2 != [] and int(parts2[j]) == 0:
+            parts2.pop()
+            j -= 1
+        
+        n, m = len(parts1), len(parts2)
+
+        for x in range(min(n, m)):
+            p1, p2 = int(parts1[x]), int(parts2[x])
+            if p1 < p2:
+                return -1
+            elif p1 > p2:
+                return 1
+                
+        if len(parts1) > len(parts2):
+            return 1
+        elif len(parts2) > len(parts1):
+            return -1
+        return 0
+        
