8359cf977f7b486d140bb836c132c689baee2ea4
Abdelrahman Khaled <52962808+Abdo404Khaled@users.noreply.github.com>
2024-05-04 16:18:01 +0300

    Time: 334 ms (98.11%) | Memory: 16.9 MB (71.59%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.py
+++ 917-boats-to-save-people/boats-to-save-people.py
@@ -11,7 +11,9 @@ class Solution(object):
         while l <= r:
             if people[l] + people[r] <= limit:
                 l += 1
-            r -= 1
+                r -= 1
+            else:
+                r -= 1
             boats += 1
         
         return boats
