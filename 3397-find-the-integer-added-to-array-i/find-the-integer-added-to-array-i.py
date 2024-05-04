225cf8645ba60f0f84f653edade1dac051d20ad0
Abdelrahman Khaled <52962808+Abdo404Khaled@users.noreply.github.com>
2024-05-04 16:34:37 +0300

    Time: 27 ms (68.84%) | Memory: 11.7 MB (13.53%) - LeetSync

---
+++ 3397-find-the-integer-added-to-array-i/find-the-integer-added-to-array-i.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def addedInteger(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: int
+        """
+        nums1.sort()
+        nums2.sort()
+
+        return nums2[0] - nums1[0]
+        
