439a769cb812b9cd302ebda4ea5e01a4d0ca2893
Abdelrahman Khaled <52962808+Abdo404Khaled@users.noreply.github.com>
2024-05-21 02:29:05 +0300

    Time: 180 ms (28.31%) | Memory: 12.7 MB (66.54%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -5,9 +5,10 @@ class Solution(object):
         :rtype: int
         """
         c = Counter(s)
-        
+
         for i, ch in enumerate(s):
             if c[ch] == 1:
                 return i
+                
         return -1
         
