b9fe69c84d25a94454e2cd81f4858de365956afb
Abdelrahman Khaled <52962808+Abdo404Khaled@users.noreply.github.com>
2024-05-28 02:46:21 +0300

    Time: 365 ms (14.29%) | Memory: 33.9 MB (28.57%) - LeetSync

--- 1611-making-file-names-unique/making-file-names-unique.py
+++ 1611-making-file-names-unique/making-file-names-unique.py
@@ -1,16 +1,22 @@
-class Solution:
-    def getFolderNames(self, names: List[str]) -> List[str]:
-        words = defaultdict(int)
+class Solution(object):
+    def getFolderNames(self, names):
+        """
+        :type names: List[str]
+        :rtype: List[str]
+        """
+        words = {}
         res = []
         for name in names:
             if name not in words:
                 words[name] = 1
                 res.append(name)
             else:
-                k = 1
-                while f"{name}({k})" in words:
+                k = words[name]
+                while name + '(' + str(k) + ')' in words:
                     k += 1
-                words[f"{name}({k})"] = 1
-                res.append(f"{name}({k})")
+                words[name + '(' + str(k) + ')'] = 1
+                res.append(name + '(' + str(k) + ')')
+                words[name] = k
         
         return res
+        
