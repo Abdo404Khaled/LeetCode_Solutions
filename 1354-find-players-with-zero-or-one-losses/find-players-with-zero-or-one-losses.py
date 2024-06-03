a45bfdacb24537f1530e559dbc25f31effbe0ea1
Abdelrahman Khaled <52962808+Abdo404Khaled@users.noreply.github.com>
2024-06-04 00:33:28 +0300

    Time: 1362 ms (87.34%) | Memory: 74.9 MB (9.09%) - LeetSync

---
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def findWinners(self, matches):
+        """
+        :type matches: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        losers = defaultdict(int)
+        players = set()
+
+        for winner, loser in matches:
+            losers[loser] += 1
+            players.add(winner)
+            players.add(loser)
+        
+        zeros, ones = [], []
+        players = sorted(players)
+        for player in players:
+            if player not in losers:
+                zeros.append(player)
+            elif losers[player] == 1:
+                ones.append(player)
+        
+        return [zeros, ones]
+
+        
