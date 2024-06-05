9e5268128e7047cf19daaf4b844008a44559e7db
Abdelrahman Khaled <52962808+Abdo404Khaled@users.noreply.github.com>
2024-06-05 03:20:12 +0300

    Time: 54 ms (15.04%) | Memory: 11.9 MB (69.92%) - LeetSync

--- 1044-find-common-characters/find-common-characters.py
+++ 1044-find-common-characters/find-common-characters.py
@@ -4,25 +4,16 @@ class Solution(object):
         :type words: List[str]
         :rtype: List[str]
         """
-        if not words:
-            return []
+        common_count = Counter(words[0])
         
-        n = len(words)
-        m = defaultdict(int)
+        for word in words[1:]:
+            word_count = Counter(word)
+            for char in common_count.keys():
+                common_count[char] = min(common_count[char], word_count[char])
+        
         res = []
-    
-        for word in words:
-            # Use a set to avoid counting duplicates within the same word
-            unique_chars = set(word)
-            for char in unique_chars:
-                m[char] += 1
-        
-        # Collect characters that appear in every word
-        for char in m:
-            if m[char] == n:
-                # Find the minimum count of the character across all words
-                min_count = min(word.count(char) for word in words)
-                res.extend([char] * min_count)
+        for char, count in common_count.items():
+            res.extend([char] * count)
         
         return res
         
