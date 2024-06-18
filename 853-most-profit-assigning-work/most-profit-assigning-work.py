79b42441b5bf3d4ec45093728b67e3380f978cc7
Abdelrahman Khaled <52962808+Abdo404Khaled@users.noreply.github.com>
2024-06-18 05:11:51 +0300

    Time: 260 ms (85.53%) | Memory: 14.1 MB (84.21%) - LeetSync

---
+++ 853-most-profit-assigning-work/most-profit-assigning-work.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def maxProfitAssignment(self, difficulty, profit, workers):
+        """
+        :type difficulty: List[int]
+        :type profit: List[int]
+        :type worker: List[int]
+        :rtype: int
+        """
+        jobs = sorted(zip(difficulty, profit))
+        workers.sort()
+        
+        i, mx, n = 0, 0, len(workers)
+        res = 0
+
+        for worker in workers:
+
+            while i < n and jobs[i][0] <= worker:
+                mx = max(mx, jobs[i][1])
+                i += 1
+            
+            res += mx
+        
+        return res
+
+
+
+
+        
+        
+
+        
