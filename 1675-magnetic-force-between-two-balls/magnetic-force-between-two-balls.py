35e7ec48af34991d72f75c56ad61898d7061af6e
Abdelrahman Khaled <52962808+Abdo404Khaled@users.noreply.github.com>
2024-06-22 13:29:42 +0300

    Time: 832 ms (77.88%) | Memory: 22.5 MB (97.03%) - LeetSync

--- 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.py
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.py
@@ -1,25 +1,32 @@
 class Solution(object):
+    def check_good(self, force, positions, target):
+        balls, previous_ball = 0, -self.INF
+
+        for position in positions:
+            if position - previous_ball >= force:
+                balls += 1
+                previous_ball = position
+        
+        return balls >= target
+
+
     def maxDistance(self, position, m):
         """
         :type position: List[int]
         :type m: int
         :rtype: int
         """
+        self.INF = 10 ** 20
         position.sort()
-        n=len(position)
-        low=1
-        high=position[-1]-position[0]
-        while low<=high:
-            mid=(low+high)//2
-            balls_placed=1
-            prev_position=position[0]
-            for i in range(1,n):
-                if position[i]-prev_position>=mid:
-                    balls_placed+=1
-                    prev_position=position[i]
-            if balls_placed>=m:
-                low=mid+1
-                highest_force=mid
+
+        l, r = 1, position[-1] - position[0]
+
+        while l < r:
+            mid = (l + r + 1) // 2
+
+            if self.check_good(mid, position, m):
+                l = mid
             else:
-                high=mid-1
-        return highest_force
+                r = mid - 1
+        
+        return l
