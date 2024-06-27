1033f6c7d7e9acdbac969ebbfd53325a0d1b9a00
Abdelrahman Khaled <52962808+Abdo404Khaled@users.noreply.github.com>
2024-06-27 23:10:27 +0300

    Time: 646 ms (18.85%) | Memory: 50.7 MB (26.18%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.py
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.py
@@ -10,10 +10,12 @@ class Solution(object):
             graph[a] += 1
             graph[b] += 1
         
-        nodes = len(graph) - 1
+        max_node = -1
+        max_edge = -1
 
         for node in graph:
-            if graph[node] == nodes:
-                return node
+            if graph[node] > max_edge:
+                max_node = node
+                max_edge = graph[node]
         
-        return 0
+        return max_node
