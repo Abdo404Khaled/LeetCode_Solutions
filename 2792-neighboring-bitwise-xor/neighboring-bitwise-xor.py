3b3a7d440c4d1ec4ddb00f7239b9c136978bcbd2
Abdelrahman Khaled <52962808+Abdo404Khaled@users.noreply.github.com>
2025-01-17 20:26:48 +0200

    Time: 56 ms (80.00%) | Memory: 15.4 MB (60.00%) - LeetSync

--- 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.py
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.py
@@ -5,23 +5,12 @@ class Solution(object):
         :rtype: bool
         """
         n = len(derived)
-        dp = {}
+        xor_sum = 0
 
-        def dfs(i, first, prev):
-            if i == n:
-                return (prev ^ first) == derived[-1]
-            if (i, first, prev) in dp:
-                return dp[(i, first, prev)]
-            current = False
-            if i == 0 or (prev ^ 0) == derived[i - 1]:
-                current |= dfs(i + 1, first if i > 0 else 0, 0)
-            if i == 0 or (prev ^ 1) == derived[i - 1]:
-                current |= dfs(i + 1, first if i > 0 else 1, 1)
-            dp[(i, first, prev)] = current
-            return current
-
-        return dfs(0, 0, 0) or dfs(0, 1, 1)
-
+        for num in derived:
+            xor_sum ^= num
+        
+        return xor_sum == 0
                 
 
 
